47e3db9d70d7e1b9b15b3c8aeed84f3bb238ee03
felipegenef <felipegenef@gmail.com>
2022-06-16 15:06:11 -0300

    test

---
+++ teste.js
@@ -0,0 +1,9 @@
+const { EC2Client, DescribeInstancesCommand } = require("@aws-sdk/client-ec2");
+const ec2Client = new EC2Client({ region: "us-east-1" });
+async function main() {
+  const data = await ec2Client.send(
+    new DescribeInstancesCommand({ InstanceIds: ["INSTANCE_ID"] })
+  );
+  console.log("Success", JSON.stringify(data));
+}
+main();
